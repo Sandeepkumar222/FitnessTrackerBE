998a6a4f85efabeab124ca30664ed07b62bd2b98
Sandeepkumar222 <83975564+Sandeepkumar222@users.noreply.github.com>
2021-10-03 17:25:49 +0530

    Update index.js
    added middlewares to extend the limits to post

--- index.js
+++ index.js
@@ -28,6 +28,8 @@ async function AppServer() {
     app.use(cors());
 
     //Middelwares
+    app.use(bodyParser.json({ limit: '50mb' }))
+    app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }))
     app.use(express.json());
 
 //     app.use((req, res, next) => {
