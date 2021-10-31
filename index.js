b98cfdba27c955cc308032295e8eda038e315172
Sandeepkumar222 <83975564+Sandeepkumar222@users.noreply.github.com>
2021-10-31 13:16:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ require("dotenv").config();
 //cors
 const cors = require("cors");
 
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
 //importing JWT to check token
 const jwt = require("jsonwebtoken");
 
@@ -27,7 +33,7 @@ async function AppServer() {
     await mongo.connect();
 
     //cors
-    app.use(cors());
+    app.use(cors(corsOptions));
 
     //Middelwares
     app.use(bodyParser.json({ limit: '50mb' }))
